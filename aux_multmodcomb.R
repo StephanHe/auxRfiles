e62b5421e154744d19c795cd13b7555d6c143395
Stephan Hemri <chs@ecgb11.ecmwf.int>
2018-04-18 15:13:46 +0000

    corrected bug in f_multmod such that zero variances do not throw an error anymore

--- aux_multmodcomb.R
+++ aux_multmodcomb.R
@@ -41,6 +41,8 @@ f_multmod <- function(input, weights) {
   SIGMA1 <- matrix(unlist(sapply(input, function(x) apply(x, 1, sd, na.rm = T),
                                  simplify = FALSE)), ncol = nyears, byrow = TRUE)
 
+  SIGMA1[SIGMA1 == 0] <- 1e-100 ## needed in order to avoid zero variances 
+  
   return(t(mapply(f_multmod2, f_mat2list(MU1), f_mat2list(SIGMA1), probslist, f_mat2list(weights))))
   
 }
